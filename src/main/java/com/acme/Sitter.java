d4046d441e93fb7d725feffdaf6d70ab01c9c6ad
James Kolean <jameskolean@yahoo.com>
2015-03-18 09:03:24 -0400

    calculateCharge() validation tests pass

--- src/main/java/com/acme/Sitter.java
+++ src/main/java/com/acme/Sitter.java
@@ -1,5 +1,6 @@
 package com.acme;
 
+import org.joda.time.Duration;
 import org.joda.time.Interval;
 
 public class Sitter {
@@ -14,7 +15,19 @@ public class Sitter {
 	 *
 	 */
 	public Integer calculateCharge(final Interval interval) throws IllegalArgumentException {
+		validateInterval(interval);
 		return null;
 	}
 
+	private void validateInterval(final Interval interval) {
+		if (interval == null) {
+			throw new IllegalArgumentException("An interval is required");
+		}
+		if (interval.getStart().getHourOfDay() < 17) {
+			throw new IllegalArgumentException("Start Time must be after 5PM");
+		}
+		if (new Duration(interval.getEnd().getMillisOfDay()).isLongerThan(Duration.standardHours(4L))) {
+			throw new IllegalArgumentException("End Time must be before 4AM");
+		}
+	}
 }
